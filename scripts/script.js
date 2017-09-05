08b9018886282e14a868eb55f6b573c56d7cdd0f
Maks Fisli <mfisli2@gmail.com>
2017-09-05 13:50:31 -0700

    added jquery onclick to toggle mobile menu.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
 	console.log("js loaded");
+	
+	$(".mobile-menu-icon").on("click", function(){
+		$(".primary-nav").toggleClass("active");
+		$(this).toggleClass("open");
+	})
 })
