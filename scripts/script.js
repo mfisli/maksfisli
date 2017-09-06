21c88710590bccc01fad79d307fabc8540acda8b
Maks Fisli <mfisli2@gmail.com>
2017-09-06 12:24:49 -0700

    Deployment broke the icons, added a cdn link to fix - but this might add bloat

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-	console.log("js loaded");
-	
 	$(".mobile-menu-icon").on("click", function(){
 		$(".primary-nav").toggleClass("active");
 		$(this).toggleClass("open");
